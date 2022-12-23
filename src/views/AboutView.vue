128055535ee6757e02a0b4332e9c29d577939683
fitri wulandari <fitriwulandarii1501@gmail.com>
2022-12-23 10:57:34 +0700

    upload new

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Biodata</h1>
+    <p>Nama    : FItri Wulandari</p>
+    <p>Nim     : 11950125070</p>
+    <p>Tanggal Lahir  : 15 Januari 2001</p>
   </div>
 </template>
